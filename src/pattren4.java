907aa367767f8bc619bcacc7b2f4c6f76927ac4e
Mr.Kumar <dilipkumer092@gmail.com>
2023-02-14 20:28:01 +0530

    java programs

---
+++ src/pattren4.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class pattren4 {
+    public static void main(String[] args) {
+        int i , j ;
+
+        for (i=1;i<=4;i++){
+
+            for (j=1;j<=4;j++){
+
+               if(i==j)
+                   System.out.print(1);
+
+               else
+                   System.out.print(0);
+
+
+            }
+            System.out.println();
+
+        }
+
+
+
+    }
+}
